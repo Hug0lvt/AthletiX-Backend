7cffc66b158cb69a6387885cc9e73ff98969b61e
Hugo LIVET <Hugo.LIVET@etu.uca.fr>
2024-05-22 13:07:26 +0200

    WIP

--- sources/Model/Comment.cs
+++ sources/Model/Comment.cs
@@ -11,7 +11,6 @@ namespace Model
         public int Id { get; set; }
         public DateTime PublishDate { get; set; }
         public Profile Publisher { get; set; }
-        public Post Post { get; set; }
         public string Content { get; set; }
         public List<Comment> Answers { get; set; } = new List<Comment>();
     }
