af96e5b073ea90f34a2331d83513e89e371496b7
Hugo LIVET <Hugo.LIVET@etu.uca.fr>
2024-05-29 19:58:36 +0200

    Update Dockerfile

--- sources/API/Program.cs
+++ sources/API/Program.cs
@@ -155,7 +155,7 @@ app.UseSwaggerUI();// NE PAS LAISSER EN PROD
 
 //app.UseHttpsRedirection(); //Casser avec Codefirst
 
-# region Post File System
+#region Post File System
 var physicalPath = Path.Combine(Directory.GetCurrentDirectory(), "athv1", "posts");
 if (!Directory.Exists(physicalPath)) Directory.CreateDirectory(physicalPath);
 app.UseStaticFiles(new StaticFileOptions
